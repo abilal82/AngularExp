144da5adef02ba3533d65c44763d8617a4406195
abilal82 <m.bilalaslam82@gmail.com>
2019-01-03 08:26:41 +0500

    Added Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,62 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+//firebase 
+import { AngularFireModule ,  } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
+//Custom imports
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
+const appRoutes : Routes = [
+  {path:'',component:DashboardComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'login', component:LoginComponent}
+]
+export const  firebaseConfig = {
+  apiKey: "AIzaSyBbBCxlNJru-K7jK-VIvMwgk5om8Ig-uy4",
+  authDomain: "clientpanel-9263.firebaseapp.com",
+  databaseURL: "https://clientpanel-9263.firebaseio.com",
+ // projectId: "clientpanel-9263",
+  storageBucket: "clientpanel-9263.appspot.com",
+  messagingSenderId: "818665339878"
+};
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ClientsComponent,
+    ClientDetailsComponent,
+    AddClientComponent,
+    EditClientComponent,
+    NavbarComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth,
+    AngularFireDatabase
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/client-service.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList };
+import { from } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientServiceService {
+
+  constructor() { }
+}
